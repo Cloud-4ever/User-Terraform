20a7170d05b16214e8d10744fe9bdfa40cea7b7c
songyeonsu <songyeonsu12@gmail.com>
2026-03-20 16:18:49 +0900

    Delete terraform directory

--- terraform/s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "images" {
-  bucket = "shopeasy-image-bucket-c4-easyshop-review-image" 
-}
-
-resource "aws_s3_bucket" "static_assets" {
-  bucket = "shopeasy-static-bucket-c4-easyshop-assets" 
-}
